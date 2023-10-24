456dfa7e915054dcac16e39a87df9d49209a7218
marinam34 <merzlyakova03@list.ru>
2023-10-24 20:45:48 +0300

    task2 attempt 1

---
+++ lab_2/Solution1/ViewModel/MainViewModel.cs
@@ -0,0 +1,141 @@
+﻿using ClassLibrary1;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public interface IUIServices
+    {
+        void ReportError(string message);
+        string? SaveFile();
+        string? OpenFile();
+
+    }
+    public class Data
+    {
+        public string Text { get; set; }
+        public string Alignment { get; set; }
+        public Data(string text, string alignment)
+        {
+            Text = text;
+            Alignment = alignment;
+        }
+    }
+
+
+    public class MainViewModel : ViewModelBase
+    {
+        public CancellationTokenSource cts { get; set; }
+
+        IProgress<double> p = new Progress<double>(progress =>
+        {
+            Console.WriteLine("Downloaded: {0:f3} Mbyte", progress);
+        });
+        private bool FileLoaded = false;
+        private bool is_running = false;
+        public string Text { get; set; }
+        private readonly IUIServices uiServices;
+        public ObservableCollection<Data> Messages { get; set; }
+        public ICommand SendCommand { get; private set; }
+        public ICommand CancelCommand { get; private set; }
+        public string Question { get; set; }
+
+        public TextAnalyzer Analyzer { get; set; }
+        public async Task Send(object arg)
+        {
+            cts = new CancellationTokenSource();
+            try
+            {
+                string quest = this.Question;
+                Question = String.Empty;
+                RaisePropertyChanged(nameof(Question));
+                is_running = true;
+                if (quest != null && quest != String.Empty)
+                {
+                    if (quest == "/load")
+                    {
+                        Messages.Add(new Data("/load", "Right"));
+                        string? fileName = uiServices.OpenFile();
+                        if (fileName != null)
+                        {
+                            Analyzer = await TextAnalyzer.CreateAsync(fileName, cts.Token, p);
+
+                            if (System.IO.File.Exists("bert-large-uncased-whole-word-masking-finetuned-squad.onnx"))
+                            {
+                                FileLoaded = true;
+                            }
+                            Messages.Add(new Data(await File.ReadAllTextAsync(fileName, cts.Token), "Left"));
+                        }
+                        else
+                        {
+                            Messages.Add(new Data("Файл не выбран, напишите команду /load для выбора файла", "Left"));
+                        }
+                    }
+                    else
+                    {
+                        Messages.Add(new Data(quest, "Right"));
+                        if (FileLoaded == false)
+                        {
+                            string? fileName = uiServices.OpenFile();
+                            if (fileName != null)
+                            {
+                                //создать объект с текстом
+                                Analyzer = await TextAnalyzer.CreateAsync(fileName, cts.Token, p);
+
+                                FileLoaded = true;
+
+                                Messages.Add(new Data(await File.ReadAllTextAsync(fileName, cts.Token), "Left"));
+                            }
+                            else
+                            {
+                                Messages.Add(new Data("Файл не выбран, напишите команду /load для выбора файла", "Left"));
+                            }
+                        }
+                        if (FileLoaded == true)
+                        {
+                           
+                            string answer = await Analyzer.GetAnswerAsync(quest, cts.Token);
+
+                            Messages.Add(new Data(answer, "Left"));
+                        }
+
+
+
+                    }
+                    
+                }
+            }
+            catch(Exception ex)
+            {
+                uiServices.ReportError(ex.Message);
+            }
+            finally
+            {
+                is_running = false;
+            }
+        }
+
+        public void Cancel(object arg)
+        {
+            cts.Cancel(); 
+        }
+
+        public MainViewModel(IUIServices uiServices)
+        {
+            FileLoaded = false;
+            
+            SendCommand = new AsyncRelayCommand(Send);
+            CancelCommand = new RelayCommand(Cancel, _ => is_running);
+            this.uiServices = uiServices;
+            Messages = new();
+        }
+    }
+}
+
---
+++ lab_2/c_sharp2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ViewModel;
+
+namespace c_sharp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IUIServices
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel(this);
+        }
+
+        public void ReportError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? SaveFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? OpenFile()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
