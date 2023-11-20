4fdf39cb33066769461e8d41234c733664e32696
marinam34 <merzlyakova03@list.ru>
2023-11-20 19:22:08 +0300

    lab3 attempt1

---
+++ lab3/Solution1/View/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ViewModel;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IUIServices
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel(this);
+        }
+
+        public void ReportError(string message)
+        {
+            errorBar.Text = message;
+            errorBar.Visibility = Visibility.Visible;
+        }
+
+        public string? SaveFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? OpenFile()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                return dlg.FileName;
+            }
+            return null;
+        }
+
+    }
+}
